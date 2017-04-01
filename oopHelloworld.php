a2803f713873e6fd3474c98b041760d43e88bf9f
Mo Leigh <moleighjackson@gmail.com>
2017-03-31 21:07:48 -0400

    clone of helloworld.php
    working copy of new oop hello world app

---
+++ oopHelloworld.php
@@ -0,0 +1,19 @@
+<html>
+    <head>
+          <title>Goodbye Universe.</title>
+          <link rel="stylesheet" type="text/css" href="hwstyle.css">
+    </head>
+    <body>
+      <h1 id="hello-world"><?php
+      echo "Hello World!";
+      ?></h1>
+    </body>
+
+    <script type="text/javascript">
+      alert('*Afternoon Moon*');
+      </script>
+      <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js" ></script>
+      <script>
+             $("h1").text("¡Hola, Mundo!");
+          </script>
+</html>
